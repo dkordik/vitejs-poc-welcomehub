46154546366209bdfb31d2b6421eddc07de1eb26
Dan Kordik <dan.kordik@cision.com>
2022-03-03 18:27:07 -0600

    Add prod build and easy way to test, and remove some demo files

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # vitejs-vite-l1pcat
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-l1pcat)
+
+Use mkcert to set up certs, as described here: https://stackoverflow.com/a/69743888
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,32 @@
-// import { defineConfig } from "vite";
-// import fs from "fs";
+import { defineConfig } from "vite";
+import fs from "fs";
+import path from "path";
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   server: {
-//     https: {
-//       // use mkcert to create these, as described here: https://stackoverflow.com/a/69743888
-//       key: fs.readFileSync("./.cert/key.pem"),
-//       cert: fs.readFileSync("./.cert/cert.pem"),
-//     },
-//   },
-// });
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    https: {
+      // use mkcert to create these, as described here: https://stackoverflow.com/a/69743888
+      key: fs.readFileSync("./.cert/key.pem"),
+      cert: fs.readFileSync("./.cert/cert.pem"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "init.welcome-hub-embedded.js"),
+      name: "WelcomeHubEmbedded",
+      fileName: () => 'init.welcome-hub-embedded.js',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+        },
+      },
+    },
+  },
+});
