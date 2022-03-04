382b0708a7e2698c3790a8d00843e4186d21da0e
Dan Kordik <dan.kordik@cision.com>
2022-03-03 18:35:55 -0600

    Remove extra rollup overrides that we aren't using

--- vite.config.js
+++ vite.config.js
@@ -17,16 +17,5 @@ export default defineConfig({
       name: "WelcomeHubEmbedded",
       fileName: () => 'init.welcome-hub-embedded.js',
     },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: [],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-        },
-      },
-    },
   },
 });
